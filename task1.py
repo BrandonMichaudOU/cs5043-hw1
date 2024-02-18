5674075074961203cebfed342d9e008f1ac4d608
Brandon Michaud <michaudbrandond@gmail.com>
2024-02-18 14:46:12 -0600

    added legend for task 1 plot

--- task1.py
+++ task1.py
@@ -10,12 +10,13 @@ def make_plot():
 
     # Create histogram
     fig = plt.figure()
-    plt.plot(obj['time_testing'], obj['predict_testing'])
-    plt.plot(obj['time_testing'], obj['actual_testing'])
+    plt.plot(obj['time_testing'], obj['predict_testing'], label='predicted')
+    plt.plot(obj['time_testing'], obj['actual_testing'], label='actual')
     plt.xlim([1310, 1317])
     plt.ylabel('Elbow Acceleration')
     plt.xlabel('Time')
     plt.title('Elbow Acceleration vs Time')
+    plt.legend()
 
     # Initialize WandB
     wandb.init(project='hw1', name='task1_figure')
