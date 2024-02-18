c771e2cba5cb5e57a6c3d8eff800d8181b11d00a
Brandon Michaud <michaudbrandond@gmail.com>
2024-02-18 15:24:37 -0600

    separated check batch file

--- check.sh
+++ check.sh
@@ -13,9 +13,8 @@
 #SBATCH --mail-user=brandondmichaud@ou.edu
 #SBATCH --mail-type=ALL
 #SBATCH --chdir=/home/cs504319/cs5043-hw1
-#SBATCH --array=0-159
 
 . /home/fagg/tf_setup.sh
 conda activate tf
 
-python hw1_base_skel.py --project 'hw1' --predict_dim 1 --exp_index $SLURM_ARRAY_TASK_ID --lrate 0.0001 --activation_hidden 'elu' --activation_out 'linear' --hidden 100 10 --epochs 100 --min_delta 0.001 --patience 25 -vv
+python hw1_base_skel.py --check
